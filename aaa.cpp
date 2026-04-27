684bd324af43597f501307d55824310340a8c829
Ariazoey <1154058759@qq.com>
2026-04-27 22:51:56 +0800

    Create aaa.cpp
    添加了一个显示操作功能

---
+++ aaa.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+	//显示菜单
+	cout<<"功能----------------" <<endl;
+	cout<<"查看所有联系人请输入1" <<endl;
+	cout<<"删除某个联系人请输入2" <<endl;
+	cout<<"增加某个联系人请输入3" <<endl;
+	return 0;
+}
